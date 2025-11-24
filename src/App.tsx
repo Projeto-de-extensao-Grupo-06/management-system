39d9e0ca4523cbd95dc3f1cd9234399f710ad123
RanierDalton <ranier.couto@sptech.school>
2025-11-24 18:29:07 -0300

    remove: Removendo o conteúdo do diretório de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import './App.css'
-import AppRoutes from './routes/AppRoutes';
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login/Login";
+import AppLayout from "./layouts/AppLayout/AppLayout";
+import Clients from "./pages/Clients/Clients";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+
+          <Route element={<AppLayout />}>
+            <Route path="/clients" element={<Clients />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
