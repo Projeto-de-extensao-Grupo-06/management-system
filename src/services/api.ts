c7119820f4ab0e5fac8ceed7e20ad27f1d5bf70e
Ranier Dalton <ranier.couto@sptech.school>
2025-11-25 20:30:23 -0300

    add: Mudando forma de consumo do .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-import { configDotenv } from 'dotenv';
-configDotenv();
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_BASE_URL,
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
