6190241e0432787895f8910bc0e1825e7087a65f
RanierCouto <ranier.couto@exedconsulting.com>
2025-12-31 13:08:07 -0300

    refactor: Refatorando validação manual de CPF e usando bilbioteca

--- src/schemas/clientSchema.ts
+++ src/schemas/clientSchema.ts
@@ -1,49 +1,5 @@
+import { cpf, cnpj } from 'cpf-cnpj-validator';
 import { z } from 'zod';
-
-const cleanString = (val: string) => val.replace(/[^\d]+/g, '');
-
-const isValidCPF = (cpf: string) => {
-    cpf = cleanString(cpf);
-    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
-    let sum = 0, remainder;
-    for (let i = 1; i <= 9; i++) sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-    remainder = (sum * 10) % 11;
-    if ((remainder === 10) || (remainder === 11)) remainder = 0;
-    if (remainder !== parseInt(cpf.substring(9, 10))) return false;
-    sum = 0;
-    for (let i = 1; i <= 10; i++) sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-    remainder = (sum * 10) % 11;
-    if ((remainder === 10) || (remainder === 11)) remainder = 0;
-    if (remainder !== parseInt(cpf.substring(10, 11))) return false;
-    return true;
-};
-
-const isValidCNPJ = (cnpj: string) => {
-    cnpj = cleanString(cnpj);
-    if (cnpj.length !== 14) return false;
-    let length = cnpj.length - 2
-    let numbers = cnpj.substring(0, length);
-    const digits = cnpj.substring(length);
-    let sum = 0;
-    let pos = length - 7;
-    for (let i = length; i >= 1; i--) {
-        sum += parseInt(numbers.charAt(length - i)) * pos--;
-        if (pos < 2) pos = 9;
-    }
-    let result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result !== parseInt(digits.charAt(0))) return false;
-    length = length + 1;
-    numbers = cnpj.substring(0, length);
-    sum = 0;
-    pos = length - 7;
-    for (let i = length; i >= 1; i--) {
-        sum += parseInt(numbers.charAt(length - i)) * pos--;
-        if (pos < 2) pos = 9;
-    }
-    result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result !== parseInt(digits.charAt(1))) return false;
-    return true;
-};
 
 export const clientSchema = z.object({
     firstName: z.string().min(1, 'Primeiro nome é obrigatório'),
@@ -61,7 +17,7 @@ export const clientSchema = z.object({
     notes: z.string().optional(),
 }).superRefine((data, ctx) => {
     if (data.documentType === 'CPF') {
-        if (!isValidCPF(data.document)) {
+        if (!cpf.isValid(data.document)) {
             ctx.addIssue({
                 code: z.ZodIssueCode.custom,
                 message: 'CPF inválido',
@@ -69,7 +25,7 @@ export const clientSchema = z.object({
             });
         }
     } else if (data.documentType === 'CNPJ') {
-        if (!isValidCNPJ(data.document)) {
+        if (!cnpj.isValid(data.document)) {
             ctx.addIssue({
                 code: z.ZodIssueCode.custom,
                 message: 'CNPJ inválido',
