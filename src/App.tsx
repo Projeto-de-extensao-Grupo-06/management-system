6bebd35181d665ac966e16136cf982564497091e
victorsantos41 <victor.santos047@sptech.school>
2026-03-14 17:43:34 -0300

    feat: endpoiint materiais

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,17 @@ import LoginLayout from "./layouts/login_layout/LoginLayout";
 import Analysis from "./pages/analysis/Analysis";
 import { ForgetPassword } from "./pages/auth/forgetPassword/ForgetPassword";
 import Login from "./pages/auth/login/Login";
+import BudgetDetails from "./pages/Budget/BudgetDetails";
 import ClientDetails from "./pages/clients/ClientDetails";
 import Clients from "./pages/clients/Clients";
 import Config from "./pages/config/Config";
+import Materials from "./pages/materials/Materials";
 import ProjectDetails from "./pages/projects/ProjectDetails";
 import ProjectNotifications from "./pages/projects/ProjectNotifications";
 import Projects from "./pages/projects/Projects";
 import Schedule from "./pages/schedule/Schedule";
-import BudgetDetails from "./pages/Budget/BudgetDetails";
 import NotFound from "./pages/shared/NotFound";
 import useAuthStore from "./store/useAuthStore";
-import ProposalPage from "./components/pdf/ProposalPage";
 
 function App() {
   const checkAuth = useAuthStore((state) => state.checkAuth);
@@ -83,6 +83,14 @@ function App() {
             <Route
               path="/projetos/:id/orcamento"
               element={<PermissionRoute permissions={["BUDGET_READ"]} element={<BudgetDetails />} />}
+            />
+            <Route
+              path="/projetos/:id/materiais"
+              element={<PermissionRoute permissions={["BUDGET_READ"]} element={<Materials />} />}
+            />
+            <Route
+              path="/materiais"
+              element={<PermissionRoute permissions={["BUDGET_READ"]} element={<Materials />} />}
             />
             <Route path="/configuracoes" element={<Config />} />
             
