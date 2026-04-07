d394f01824b3f78c0c7ca01881e69a943e79141c
RanierCouto <ranier.couto@exedconsulting.com>
2026-04-07 18:10:06 -0300

    fix: Ajustando chamada do path da tela de configuração

--- src/pages/config/Config.tsx
+++ src/pages/config/Config.tsx
@@ -57,7 +57,7 @@ export default function Config() {
       title: "Meu Perfil",
       description: "Edite informações pessoais",
       icon: faUser,
-      path: "/configuracoes/configPerfil",
+      path: "/configuracoes/config-perfil",
     },
     {
       id: 6,
