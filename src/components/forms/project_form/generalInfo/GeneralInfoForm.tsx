adfddf6dcca186958eaa5dfd89c2e774a93f0d63
unknown <ranier.couto@sptech.school>
2026-05-09 17:58:50 -0300

    fix: Ajustando erros de build

--- src/components/forms/project_form/generalInfo/GeneralInfoForm.tsx
+++ src/components/forms/project_form/generalInfo/GeneralInfoForm.tsx
@@ -67,113 +67,6 @@ export default function GeneralInfoForm({ project, setProject }: GeneralInfoForm
         });
         await patchProject({ responsibleId: id });
     }
-  }
-
-  function projectNameHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value;
-
-    setProject((prev) => {
-      if (!prev) return prev;
-      return { ...prev, name: value };
-    });
-  }
-
-  async function projectNameBlurHandler() {
-    await patchProject({ name: project.name });
-  }
-
-  async function projectSystemTypeChangeHandler(value: string) {
-    setProject((prev) => {
-      if (!prev) return prev;
-      return { ...prev, systemType: value as any };
-    });
-
-    await patchProject({
-      projectType: value as "ON_GRID" | "OFF_GRID",
-    });
-  }
-
-  async function coworkerChangeHandler(v: AutoCompleteSelectOption | null) {
-    const id = Number.parseInt(v?.value ?? "0");
-
-    setProject((prev) => {
-      if (!prev) return prev;
-      return { ...prev, coworkerId: id };
-    });
-
-    await patchProject({ responsibleId: id });
-  }
-
-  useEffect(() => {
-    const loadCoworkers = async () => {
-      const coworkersResponse = await coworkerService.getAllCoworkers();
-
-      const coworkers = Array.isArray(coworkersResponse)
-        ? coworkersResponse
-        : (coworkersResponse.content ?? []);
-
-      if (coworkers.length > 0) {
-        const coworkersLabels = coworkers.map((c) => ({
-          value: c.id.toString(),
-          label: `${c.firstName} ${c.lastName}`,
-        }));
-
-        setCoworkersSelectOptions(coworkersLabels);
-      }
-    };
-
-    loadCoworkers();
-  }, []);
-
-  const projectSystemTypeOptions = useMemo(
-    () => [
-      { value: "OFF_GRID", label: "Off-Grid" },
-      { value: "ON_GRID", label: "On-Grid" },
-    ],
-    [],
-  );
-
-  const projectStatusOptions = useMemo(
-    () => [
-      { value: "NEW", label: "Novo" },
-      { value: "PRE_BUDGET", label: "Pré-orçamento" },
-      {
-        value: "CLIENT_AWAITING_CONTACT",
-        label: "Aguardando contato com cliente",
-      },
-      { value: "AWAITING_RETRY", label: "Aguardando nova tentativa" },
-      { value: "RETRYING", label: "Realizando nova tentativa de contato" },
-      { value: "SCHEDULED_TECHNICAL_VISIT", label: "Visita técnica agendada" },
-      { value: "TECHNICAL_VISIT_COMPLETED", label: "Visita técnica concluída" },
-      { value: "FINAL_BUDGET", label: "Orçamento final" },
-      { value: "AWAITING_MATERIALS", label: "Aguardando materiais" },
-      { value: "SCHEDULED_INSTALLING_VISIT", label: "Instalação agendada" },
-      { value: "INSTALLED", label: "Instalado" },
-      { value: "COMPLETED", label: "Concluído" },
-      { value: "NEGOTIATION_FAILED", label: "Negociação não concluída" },
-    ],
-    [],
-  );
-
-  return (
-    <div>
-      <div className={styles.infoContainer}>
-        <FontAwesomeIcon icon={faInfoCircle} color="#FFC300" size="xl" />
-        <span className={styles.infoText}>Informações Gerais</span>
-      </div>
-
-      <div className={styles.form}>
-        <div className={styles.inputContainer}>
-          <label className={styles.inputLabel}>Nome do projeto:</label>
-          <Input
-            disabled={!canEdit}
-            value={project.name}
-            onChange={projectNameHandler}
-            onBlur={projectNameBlurHandler}
-            placeholder="Nome do projeto..."
-          />
-        </div>
-
     async function handleStatusChange(newStatus: ProjectStatusType) {
         const previousStatus = project.status;
 
@@ -240,31 +133,19 @@ export default function GeneralInfoForm({ project, setProject }: GeneralInfoForm
 
     useEffect(() => {
         const loadCoworkers = async () => {
-            const coworkers = await coworkerService.getAllCoworkers();
+            const coworkersResponse = await coworkerService.getAllCoworkers();
 
-        <div className={styles.inputContainer}>
-          <label className={styles.inputLabel}>Tipo de Instalação:</label>
-          <Select
-            disabled={!canEdit}
-            value={project.systemType}
-            onChange={projectSystemTypeChangeHandler}
-          >
-            {projectSystemTypeOptions.map((v, key) => (
-              <SelectOption label={v.label} value={v.value} key={key} />
-            ))}
-          </Select>
-        </div>
+            const coworkers = Array.isArray(coworkersResponse)
+                ? coworkersResponse
+                : (coworkersResponse.content ?? []);
 
-        <div className={styles.inputContainer}>
-          <label className={styles.inputLabel}>Responsável:</label>
-          <AutoCompleteSelect
-            isDisabled={!canEdit}
-            options={coworkerSelectOptions}
-            onChange={coworkerChangeHandler}
-            value={
-              coworkerSelectOptions.find(
-                (o) => o.value === project.coworkerId?.toString(),
-              ) ?? null
+            if (coworkers.length > 0) {
+                const coworkersLabels = coworkers.map((c) => ({
+                    value: c.id.toString(),
+                    label: `${c.firstName} ${c.lastName}`,
+                }));
+
+                setCoworkersSelectOptions(coworkersLabels);
             }
         };
 
@@ -356,7 +237,5 @@ export default function GeneralInfoForm({ project, setProject }: GeneralInfoForm
                 </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
